ea0917d003e1869dcc87ea5c59759ec6da82e654
Talha Imam <talhaimam1702@gmail.com>
2025-05-21 23:26:45 -0400

    added tree-sitter, telescope, nvim tree, line indents

--- init.lua
+++ init.lua
@@ -4,5 +4,8 @@ vim.cmd("set tabstop =2")
 vim.cmd("set shiftwidth =2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
 
 require("config.lazy")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,68 @@
-require("lazy").setup({{
+return{
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function () 
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "cpp", "verilog", "systemverilog"},
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "cpp", "verilog"},
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
         })
     end
- }})
+  },
+
+  {
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+    config = function ()
+      vim.cmd.colorscheme("catppuccin-macchiato") --latte, frappe, macchiato, mocha
+    end
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
+
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })  
+    end
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+      require("lualine").setup{
+        option = { theme = ayu_mirage },
+      }
+    end
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
+}
